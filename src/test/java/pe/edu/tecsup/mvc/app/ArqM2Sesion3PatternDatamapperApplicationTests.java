72e0b1a81c965ab54e45d4860f744d489d34057f
jgomezz <jgomezz@gmail.com>
2026-05-04 23:01:48 -0500

    first version

---
+++ src/test/java/pe/edu/tecsup/mvc/app/ArqM2Sesion3PatternDatamapperApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.mvc.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqM2Sesion3PatternDatamapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
